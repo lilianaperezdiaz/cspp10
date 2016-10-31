87b19e99f97d13507ac275f87fe77b1037e65870
Liliana Perez Diaz <lilianaperezdiaz@uagateway.org>
2016-10-31 16:13:52 +0000

    worked on number guess game

--- Unit 3/Lperez_mockatm.py
+++ Unit 3/Lperez_mockatm.py
@@ -1,20 +1,18 @@
 bank_account = 10000
 print("1. Withdraw \n2. Deposit \n3. Exit")
 choice = input("Welcome to ATM! Pick from above [1|2|3]:")
-while choice != 3: #student completes while loop
+while choice != "3": 
     if choice == "1":
         withdrawl=input("What is the amount you would like to withdraw?")
-        #user chooses 'withdraw'
-        #student does this part
-        bank_account = bank_account - withdrawl
+        bank_account = bank_account - int(withdrawl)
     elif choice == "2":
         amount = input("How much to deposit: ")
-        bank_account = bank_account + amount
+        bank_account = bank_account + int(amount)
     elif choice == "3":
-        #user chooses 'exit'
         exit=input("Are you sure you would like to exit?")
-        #student does this part too
+    else:
         break
-
+    
+    print("Balance: {}".format(bank_account))
     print("1. Withdraw \n2. Deposit \n3. Exit")
     choice = input("Pick from above [1|2|3]:")
