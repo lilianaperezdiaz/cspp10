d9b10981c8739d7706449efb2c0b024a0de42016
Liliana Perez Diaz <lilianaperezdiaz@uagateway.org>
2016-11-02 16:04:16 +0000

    finnished the game

--- Unit 3/Lperez_numguess.py
+++ Unit 3/Lperez_numguess.py
@@ -1,18 +1,21 @@
 import random
 randomnum = (random.randint(0,101))
 num = int(input("Guess a number between 1-100: "))
-while num != randomnum:
+amount= 0
+while num >= 1 and num <= 100:
     if num < randomnum:
         print("Higher! Guess again!")
-        num = num +1
+        num = int(input("Guess a number between 1-100: "))
+        amount = amount + 1
+        
     elif num > randomnum:
         print("Lower! Guess agian!")
-    else:
-        break
-    num = int(input("Guess a number between 1-100: "))
+        num = int(input("Guess a number between 1-100: "))
+        amount = amount + 1
     
-    if num == randomnum:
+    elif num == randomnum:
         print("You got it!")
-    
+        print(" It took you {} tries".format(amount))
+        break
         
 
