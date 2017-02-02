40caf0660da3a51c0575a343b764d802696269f1
Liliana Perez Diaz <lilianaperezdiaz@uagateway.org>
2017-02-02 17:13:28 +0000

    worked on suto code to write my hard code

--- unit5/Lperez_scramble.py
+++ unit5/Lperez_scramble.py
@@ -4,12 +4,21 @@ word="issues"
 #function name: scramble_word()
 #   arguments:word
 #   purpose:keep the first and the last letters and scarble the rest, use random.shuffle()
+#           make the string a list and use the index to keep the first and last letters in place
+#           and print the final crambled word
 #   returns:the scarbled word
 def scramble_word(word):
+    list(word)
+    same_letter=word[0:-1]
+    random.shuffle(word)
+    print("{}".format(same_letter+word))
+    return word
     
 
-#variable name: sentence
+sentence="The tree threw three tanks today."
 #function name: scramble_phrase
 #   argument:sentence
 #   purpose:scramble the phrase, use string.split()
 #   return:the scrambled phrase
+def scramble_phrase(sentence):
+    sentence.split("t")
