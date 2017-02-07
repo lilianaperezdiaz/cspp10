22ea14426a267382b64fe3c9d141e5739281ed7f
Liliana Perez Diaz <lilianaperezdiaz@uagateway.org>
2017-02-07 03:05:33 +0000

    wokred on the functions of my scramble based in suto code.

--- unit5/Lperez_scramble.py
+++ unit5/Lperez_scramble.py
@@ -1,21 +1,25 @@
 import random
-word="issues"
+word="music"
 
 #function name: scramble_word()
 #   arguments:word
 def scramble_word(word):
     #make the string a list so that you can use the index
-    list(word)
+    split=list(word)
     #remove the first and last letters of the word so that they are not scrambled
-    same_letter=word[0:-1]
+    first_letter=split[0]
+    last_letter=split[-1]
+    #same the rest of the letters in a variable
+    shuffle_letters=split[0:-1]
     #shuffle the rest of the letters left
-    random.shuffle(word)
+    random.shuffle(shuffle_letters)
     #put the word back together
-    shuffled_word=same_letter+word
-    #print the word
-    print("{}".format(shuffled_word)
-    #return the word
-    return (word)
+    shuffle_letters.insert(0,first_letter)
+    shuffle_letters.append(last_letter)
+    final="".join(shuffle_letters)
+    print(final)
+scramble_word(word)
+
     
 
 sentence="The tree threw three tanks today."
@@ -25,3 +29,4 @@ sentence="The tree threw three tanks today."
 #   return:the scrambled phrase
 def scramble_phrase(sentence):
     sentence.split("t")
+    
